dcb7db9ce00e6a3bc2e2607bee9e459ffddf16f1
GoodWoofie <kufelelo@gmail.com>
2021-11-05 10:52:59 +0100

    Saves Login Password and Site

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
+function init() {
+load()
+}
 function save() {
 var login = document.getElementById("login").value;
 var password = document.getElementById("passwd").value;
 var site = document.getElementById("site").value;
 var data = [login, password, site];
-localStorage.setItem('data', JSON.stringify(data));
+
+var oldData = JSON.parse(localStorage.getItem('data'));
+if(oldData == null)
+    oldData = Array();
+oldData.push(data);
+localStorage.setItem('data', JSON.stringify(oldData));
+load();
+}
+function load() {
+    let tableDiv = document.getElementById('table');
+    let dataArray = JSON.parse(window.localStorage.getItem('data'));
+    let buffer = "";
+            buffer += "<ul>";
+            for(let i=0; i< dataArray.length; i++) {
+                buffer += "<li>login: "+dataArray[i][0]+" Hasło: "+dataArray[i][1]+" Strona: "+dataArray[i][2]+"</li>";
+            }
+            buffer += "</ul>";
+            tableDiv.innerHTML = buffer;
 }
